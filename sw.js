39ace41e5d568c68ec61cc1ec823f1ae854f0e77
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-01 19:22:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='battle-arcade-v1';
+const ASSETS=['.','./index.html','./manifest.webmanifest','./icon-180.png','./assets/placeholder.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
