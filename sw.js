5c41be71e3768f2c361403d6a80479cf08fa77ae
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-01 21:57:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'necro-v2';
+const CACHE = 'necro-v3';
 const ASSETS = ['.', 'index.html','styles.css','app.js','manifest.webmanifest'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
