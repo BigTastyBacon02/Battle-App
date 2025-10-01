c52d94d66ded5b46e16860a5cf0caf0f4c5b876f
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-01 22:02:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
 const CACHE = 'necro-v3';
-const ASSETS = ['.', 'index.html','styles.css','app.js','manifest.webmanifest'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+const ASSETS = ['.', 'index.html', 'style.css', 'game.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => {
+        if (k !== CACHE) return caches.delete(k);
+      }))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
