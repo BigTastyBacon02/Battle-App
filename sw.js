ddf6449c54de4ef512ae422542e574d3deb3e9d7
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-01 20:31:07 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='as-retro80s-v1';
-const ASSETS=['.','./index.html','./style.css','./main.js','./manifest.webmanifest','./icon-180.png','./assets/placeholder.png','./assets/logo.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
